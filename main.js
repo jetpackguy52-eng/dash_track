75afe15ed78e496f07348dbf55152f3efddc8a9e
calhounbryce13 <jetpackguy52@gmail.com>
2025-08-18 17:11:59 -0700

    updates

--- main.js
+++ main.js
@@ -2,5 +2,50 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+    if(!(localStorage.getItem('counter'))){
+        const counterObject = {
+            count: 10
+        }
+        localStorage.setItem('counter', JSON.stringify(counterObject));
+    }
+
+    render_count_on_page();
+
+    const decrementButton = document.getElementById('dec');
+    decrementButton.addEventListener('click', () => {
+        const current = get_current_count();
+        update_local_storage(current - 1);
+        render_count_on_page();
+    });
+
+
+    const resetButton = document.getElementById('reset');
+    resetButton.addEventListener('click', () => {
+        update_local_storage(10);
+        render_count_on_page();
+    });
 })
+
+
+
+
+const get_current_count = function(){
+    const current = JSON.parse(localStorage.getItem('counter')).count;
+    return current;
+}
+
+
+
+
+const update_local_storage = function(newCount){
+    const counterObject = {
+        count: newCount
+    }
+    localStorage.setItem('counter', JSON.stringify(counterObject));
+}
+
+const render_count_on_page = function(){
+    let count = JSON.parse(localStorage.getItem('counter')).count;
+    const textElement = document.getElementById('counter');
+    textElement.textContent = count;
+}
